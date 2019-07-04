da2a2c5ce7f4cd9481d42a969b8810402f5ca2ae
MoonDrinkWind <tanchengzhang520@qq.com>
2019-07-04 14:13:07 +0800

    2019.9.4.14.12

---
+++ src/main/java/china/moondrinkwind/minecraft/team/windguild/WindGuild.java
@@ -0,0 +1,17 @@
+package china.moondrinkwind.minecraft.team.windguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WindGuild extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

4c0c0c86201584030f2ab93033de15085e8b783c
MoonDrinkWind <tanchengzhang520@qq.com>
2019-07-04 15:14:57 +0800

    2019.9.4.15.14

--- src/main/java/china/moondrinkwind/minecraft/team/windguild/database/SqlManager.java
+++ src/main/java/china/moondrinkwind/minecraft/team/windguild/database/SqlManager.java
@@ -5,5 +5,29 @@ package china.moondrinkwind.minecraft.team.windguild.database;
  * @Description All sql will save in this class
  * */
 public enum SqlManager {
+    createGuildTable("createGuildTable","create table Guild(Name CHAR(25) NOT NULL,Owner CHAR(25) NOT NULL,Prefix CHAR(25) NOT NULL)"),
+    createPlayerTable("createPlayerTable","create table Guild_Player(Name CHAR(25)NOT NULL,BelongGuild CHAR(25))"),
+    getGuild("getGuild","select Name,Owner,Prefix from Guild where Name = ?"),
+    getBelongGuild("getBelong","select BelongGuild from Guild_Player where Name = ?"),
+    deleteGuild("deleteGuild","delete Guild where Name = ?"),
+    updateGuild("updateGuild","update Guild set Owner = ? , Prefix = ?"),
+    insertGuild("insertGuild","insert Guild values(?,?,?)"),
+    insertPlayer("insertPlayer","insert Guild_Player values(?,?)");
 
+    private String name;
+    private String sql;
+
+    SqlManager(String name,String sql){
+        this.name = name;
+        this.sql = sql;
+    }
+
+    public static String getSql(String name){
+        for(SqlManager sqlManager:SqlManager.values()){
+            if(sqlManager.name.equalsIgnoreCase(name)){
+                return sqlManager.sql;
+            }
+        }
+        return null;
+    }
 }
